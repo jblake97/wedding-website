2ee488213600072f9d0fbbc5c2c2c252af513537
jblake97 <jblakesinger@gmail.com>
2023-07-05 17:43:05 -0400

    added password protection

---
+++ password.js
@@ -0,0 +1,24 @@
+
+function validate()
+{
+    let input = document.getElementById("password").value;
+    //console.log(input);
+    
+    if(input == 'latte')
+    {
+        //console.log('success!');
+        let body = document.getElementById('websiteBody');
+        //console.log(body);
+
+        if (body.style.display == 'none') {
+            body.removeAttribute('style');
+            console.log(body);
+            console.log('are we in here?');
+         } 
+
+        let passDiv = document.getElementById('passDiv');
+        passDiv.style.display = 'none';
+
+    }
+    
+}
